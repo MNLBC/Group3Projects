6fc45dce4ed8bcd2d0e336eaf9b7a06dc871545b
cuntaal <alexis.cuntapay@oocl.com>
2016-06-20 20:13:27 +0800

    Commit Homework1 Week2

---
+++ Alexis_Cuntapay_Projects/W2D1_HomeWork1/W2D1/src/Worker.java
@@ -0,0 +1,102 @@
+/**
+ * 
+ * @author CUNTAAL
+ * @version 1
+ */
+
+public class Worker implements Comparable {
+
+	private int age;
+	private String name;
+	private double salary;
+
+	public Worker() {
+	}
+
+	public Worker(String name, int age, double salary) {
+		this.name = name;
+		this.age = age;
+		this.salary = salary;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public double getSalary() {
+		return salary;
+	}
+
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+
+	public void work() {
+		System.out.println(name + "is working");
+
+	}
+
+	public Worker next() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + age;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		long temp;
+		temp = Double.doubleToLongBits(salary);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Worker other = (Worker) obj;
+		if (age != other.age)
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (Double.doubleToLongBits(salary) != Double
+				.doubleToLongBits(other.salary))
+			return false;
+		return true;
+	}
+    //Method for Collecion Sort
+	@Override
+	public int compareTo(Object o) {
+		Worker otherworker = (Worker) o;
+		double salary = otherworker.getSalary();
+		if (this.getSalary() > salary) {
+			return -1;
+		} else if (this.getSalary() < salary) {
+			return 1;
+		} else {
+			return 0;
+		}
+	}
+}
