01d71bfc74d753a8a421d156dea9e698ee3b462f
rehaname <yu_melvinrobert@outlook.com>
2016-07-15 11:24:08 +0800

    InitialPush

---
+++ W5D5_Project/json/products.js
@@ -0,0 +1,20 @@
+{
+	"success": true,
+	"messageKey": "get.products",
+	"data": {
+		"products": [{
+			"productId": 1001,
+			"productName": "Bag",
+			"productDescription": "black bag",
+			"productPrice": 400.0,
+			"productStockAmount": 10
+		}, {
+			"productId": 1002,
+			"productName": "Phone",
+			"productDescription": "IPhone",
+			"productPrice": 5000.0,
+			"productStockAmount": 12
+		}]
+
+	}
+}
