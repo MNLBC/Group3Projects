ddac229131e6ed50ce4e88b631a36a6c28d115ff
brionse <sebastian.briones@oocl.com>
2016-07-18 00:44:48 +0800

    untracked files on master: 29808d6 update

---
+++ W5D5_Project/project/KanenOS/src/main/java/com/oocl/mnlbc/service/OrderDetailsMapper.java
@@ -0,0 +1,27 @@
+package com.oocl.mnlbc.service;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.springframework.jdbc.core.RowMapper;
+
+import com.oocl.mnlbc.model.CartItemBean;
+
+public class OrderDetailsMapper implements RowMapper<CartItemBean> {
+
+	@Override
+	public CartItemBean mapRow(ResultSet rs, int rowNum) throws SQLException {
+		CartItemBean item = new CartItemBean();
+		item.setOrderId(rs.getLong("ORDER_ID"));
+		item.setProductId(rs.getInt("PRODUCT_ID"));
+		item.setProductName(rs.getString("PRODUCT_NAME"));
+		item.setProductDescription(rs.getString("PRODUCT_DESCRIPTION"));
+		item.setQuantity(rs.getInt("QUANTITY"));
+		item.setProductPrice(rs.getDouble("PRODUCT_PRICE"));
+		
+	      return item;
+		
+	}
+
+}
+
