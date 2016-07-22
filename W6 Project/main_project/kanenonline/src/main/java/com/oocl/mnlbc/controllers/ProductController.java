143504f0c332132c0c0b5e5051c83b80c2212111
brionse <sebastian.briones@oocl.com>
2016-07-22 09:27:49 +0800

    generic response

--- W6 Project/main_project/kanenonline/src/main/java/com/oocl/mnlbc/controllers/ProductController.java
+++ W6 Project/main_project/kanenonline/src/main/java/com/oocl/mnlbc/controllers/ProductController.java
@@ -10,9 +10,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.oocl.mnlbc.dao.ProductDAO;
 import com.oocl.mnlbc.entity.Product;
 import com.oocl.mnlbc.model.ProductList;
-import com.oocl.mnlbc.service.ProductService;
+import com.oocl.mnlbc.model.Response;
 import com.oocl.mnlbc.utils.CollectionUtils;
 
 /**
@@ -25,7 +26,7 @@ import com.oocl.mnlbc.utils.CollectionUtils;
 public class ProductController {
 
 	@Autowired
-	private ProductService productService;
+	private ProductDAO productService;
 
 	private static final Logger logger = Logger.getLogger(ProductController.class);
 
@@ -37,21 +38,24 @@ public class ProductController {
 	 */
 	@RequestMapping(value = "/productList", method = { RequestMethod.POST })
 	@ResponseBody
-	public ProductList getProductList() throws IOException {
+	public Response<ProductList> getProductList() throws IOException {
 		logger.info("Retrieving products from the database..");
 
 		ProductList products = new ProductList();
 		List<Product> productsList = productService.getProductList();
 
 		products.setProducts(productsList);
-
+         Response<ProductList> response= new Response<ProductList>();
+         response.setData(products);
 		if (CollectionUtils.isNotEmptyList(productsList)) {
+			response.setSuccess(true);
 			logger.info("Products successfully retrieved.");
 		} else {
+			
 			logger.info("Retrieval of products failed.");
 		}
 
-		return products;
+		return response;
 	}
 
 }
